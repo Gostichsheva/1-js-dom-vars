0de0b7f123bdc33d31e8a42cd266423f8a62dbd3
Gostichsheva <violka9323@gmail.com>
2016-12-09 16:11:36 +0300

    Update list.js

--- list.js
+++ list.js
@@ -1,31 +1,31 @@
-"use strict";
+'use strict';
 
 var fragment = document.createDocumentFragment();
-var ul = document.createElement("ul");
+var ul = document.createElement('ul');
 var names = [{
-    name: "Вася",
-    sex: "male"
+    name: 'Вася',
+    sex: 'male'
 }, {
-    name: "Дима",
-    sex: "male"
+    name: 'Дима',
+    sex: 'male'
 }, {
-    name: "Катя",
-    sex: "female"
+    name: 'Катя',
+    sex: 'female'
 }, {
-    name: "Маша",
-    sex: "female"
+    name: 'Маша',
+    sex: 'female'
 }, {
-    name: "Сережа",
-    sex: "male"
+    name: 'Сережа',
+    sex: 'male'
 }, {
-    name: "Оля",
-    sex: "female"
+    name: 'Оля',
+    sex: 'female'
 }, {
-    name: "Паша",
-    sex: "male"
+    name: 'Паша',
+    sex: 'male'
 }];
-var jobs = ["Слесарь", "Менеджер", "Премьер Министр"];
-var female = ["Катя", "Маша", "Оля"];
+var jobs = ['Слесарь', 'Менеджер', 'Премьер Министр'];
+var female = ['Катя', 'Маша', 'Оля'];
 
 function getRandom(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -33,21 +33,21 @@ function getRandom(min, max) {
 
 function setLiStyle(obj, li) {
     if (obj.salary.value > 50 && obj.salary.value < 80) {
-        li.style.background = "yellow";
+        li.style.background = 'yellow';
     }
     else if (obj.salary.value < 50) {
-        li.style.background = "red";
+        li.style.background = 'red';
     }
     else {
-        li.style.background = "green";
+        li.style.background = 'green';
     }
 
-    if (obj.job.value === "Премьер Министр") {
-        li.style.textDecoration = "underline";
+    if (obj.job.value === 'Премьер Министр') {
+        li.style.textDecoration = 'underline';
     }
 
-    if (obj.sex === "female") {
-        li.style.fontSize = "150%";
+    if (obj.sex === 'female') {
+        li.style.fontSize = '150%';
     }
 }
 
@@ -56,31 +56,31 @@ for (var i = 0; i < 10; i++) {
     var obj = {
             name: {
                 value: human.name,
-                title: "ФИО"
+                title: 'ФИО'
             },
             sex: human.sex,
             age: {
                 value: getRandom(20, 35),
-                title: "Возраст"
+                title: 'Возраст'
             },
             job: {
                 value: jobs[getRandom(0, jobs.length - 1)],
-                title: "Профессия"
+                title: 'Профессия'
             },
             salary: {
                 value: getRandom(20, 120),
-                title: "Зарплата"
+                title: 'Зарплата'
             },
         },
-        li = document.createElement("li");
+        li = document.createElement('li');
 
     for (var key in obj) {
-        if (key === "sex") continue;
-        var div = document.createElement("div");
-        if (key === "name" && obj.age.value > 20 && obj.age.value < 27) {
-            div.style.fontWeight = "bold";
+        if (key === 'sex') continue;
+        var div = document.createElement('div');
+        if (key === 'name' && obj.age.value > 20 && obj.age.value < 27) {
+            div.style.fontWeight = 'bold';
         }
-        div.innerHTML = obj[key].title + ": " + obj[key].value;
+        div.innerHTML = obj[key].title + ': ' + obj[key].value;
         li.appendChild(div);
     }
 
